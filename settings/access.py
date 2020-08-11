95edb5bdb24965c804f8b1143998fd37cf847256
Timur Likhomanov <lihomanov.t@gmail.com>
2020-08-12 01:36:12 +0700

    change access file

--- settings/access.py
+++ settings/access.py
@@ -1,12 +1,14 @@
+import os
+
 postgres = {
-    'database': 'bot-test',
-    'user': 'postgres',
-    'password': 'masterkey',
-    'host': 'localhost',
-    'port': 5433
+    'database': os.environ['DATABASE'],
+    'user': os.environ['USER'],
+    'password': os.environ['PASSWORD'],
+    'host': os.environ['HOST'],
+    'port': os.environ['PORT']
 }
 
 vk = {
-    'token': 'd59d9c293e141282e9888280161eccec554ae2d5d2756e0a2cf046fb9998dc1f25bf654429c292734d03e',
-    'group_id': '197296135'
+    'token': os.environ['TOKEN'],
+    'group_id': os.environ['GROUP_ID']
 }
