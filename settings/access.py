706009a59c5035005d2c42a1387327454ad7b191
Timur Likhomanov <lihomanov.t@gmail.com>
2020-08-12 01:54:03 +0700

    finish deploy

--- settings/access.py
+++ settings/access.py
@@ -1,19 +1,12 @@
 import os
 
-print('Database ', os.environ['DATABASE'])
-print('USER ', os.environ['USER'])
-print('PASS ', os.environ['PASSWORD'])
-print('HOST ', os.environ['HOST'])
-print('PORT ', os.environ['PORT'])
-print('TOKEN ', os.environ['TOKEN'])
-print('GROUP_ID ', os.environ['GROUP_ID'])
 
 postgres = {
     'database': os.environ['DATABASE'],
     'user': os.environ['USER'],
     'password': os.environ['PASSWORD'],
     'host': os.environ['HOST'],
-    'port': int(os.environ['PORT'])
+    'port': int(os.environ['PORT_DB'])
 }
 
 vk = {
