af4569ef442339dd6ddda768390c2dcff8315474
Timur Likhomanov <lihomanov.t@gmail.com>
2020-08-12 01:51:57 +0700

    debug heroku

--- settings/access.py
+++ settings/access.py
@@ -1,5 +1,13 @@
 import os
 
+print('Database ', os.environ['DATABASE'])
+print('USER ', os.environ['USER'])
+print('PASS ', os.environ['PASSWORD'])
+print('HOST ', os.environ['HOST'])
+print('PORT ', os.environ['PORT'])
+print('TOKEN ', os.environ['TOKEN'])
+print('GROUP_ID ', os.environ['GROUP_ID'])
+
 postgres = {
     'database': os.environ['DATABASE'],
     'user': os.environ['USER'],
