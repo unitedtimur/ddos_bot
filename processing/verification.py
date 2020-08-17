1751a8687c9c5ba435d39d95c3543e672fc75f90
Timur Likhomanov <lihomanov.t@gmail.com>
2020-08-17 15:28:39 +0700

    smth

--- processing/verification.py
+++ processing/verification.py
@@ -10,7 +10,7 @@ def is_group_member(user_id) -> bool:
 
 
 def exists_user(user_id) -> None:
-    if get_user(user_id) is None:
+    if get_user(user_id) is None: 
         name, surname = get_fullname_by_user_id(user_id)
         add_user(user_id, name, surname, 'user')
 
